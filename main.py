f412ea7da3597f8bae4b9dfa66f38c5c213f9644
Mruthunjai Govindaraju <mruthunjai2003@gmail.com>
2023-12-18 15:29:20 +0530

    comments and stuff
    i added comments so yall can understand whats going on

--- main.py
+++ main.py
@@ -3,52 +3,76 @@ import librosa
 import numpy as np
 import tensorflow as tf
 
-# Constants
-FORMAT = pyaudio.paFloat32
-CHANNELS = 1
-RATE = 16000  # Same as your model's expected input
-CHUNK = 1024  # Number of audio frames per buffer
-FEATURE_TYPE = 'log_mel'  # Adjust based on your model's input
+class RealTimeAudioProcessor:
+    """
+    Real-time audio processor for handling and processing audio stream.
 
-# Initialize PyAudio
-audio_interface = pyaudio.PyAudio()
+    Attributes:
+        format (int): Audio format.
+        channels (int): Number of audio channels.
+        rate (int): Sampling rate.
+        chunk (int): Number of audio frames per buffer.
+        feature_type (str): Type of feature extraction to apply.
+        model (tf.keras.Model): Loaded TensorFlow model for predictions.
 
-# Function to preprocess audio for the model
-def preprocess_audio_for_model(audio_frame, sr=RATE, feature_type=FEATURE_TYPE):
-    if feature_type == 'log_mel':
-        # Here, implement the preprocessing as required by your model
-        # For example, converting to Mel spectrogram as shown in your original code
-        mel_spec = librosa.feature.melspectrogram(y=audio_frame, sr=sr, n_mels=40)
-        log_mel_spec = librosa.power_to_db(mel_spec)
-        return log_mel_spec.T
-    # Add other feature types if necessary
+    Methods:
+        preprocess_audio: Preprocesses audio data for model prediction.
+        stream_callback: Handles incoming audio stream and performs predictions.
+        start_stream: Starts the audio stream for real-time processing.
+    """
+    def __init__(self, model_path, format=pyaudio.paFloat32, channels=1, rate=16000, chunk=1024, feature_type='log_mel'):
+        self.format = format
+        self.channels = channels
+        self.rate = rate
+        self.chunk = chunk
+        self.feature_type = feature_type
+        self.model = tf.keras.models.load_model(model_path)
+        self.audio_interface = pyaudio.PyAudio()
 
-# Function to handle incoming audio stream
-def stream_callback(in_data, frame_count, time_info, status):
-    audio_data = np.fromstring(in_data, dtype=np.float32)
-    processed_data = preprocess_audio_for_model(audio_data)
+    def preprocess_audio(self, audio_frame):
+        """Preprocesses the audio frame for model prediction."""
+        if self.feature_type == 'log_mel':
+            mel_spec = librosa.feature.melspectrogram(y=audio_frame, sr=self.rate, n_mels=40)
+            log_mel_spec = librosa.power_to_db(mel_spec)
+            return np.expand_dims(log_mel_spec.T, axis=0)  # Ensure the shape matches model's input
+        # Additional preprocessing methods can be added here
 
-    # TODO: Add your model prediction logic here
-    # For example:
-    # transcript = model.predict(processed_data)
+    def stream_callback(self, in_data, frame_count, time_info, status):
+        """Callback function to handle incoming audio stream."""
+        audio_data = np.frombuffer(in_data, dtype=np.float32)
+        processed_data = self.preprocess_audio(audio_data)
 
-    return (in_data, pyaudio.paContinue)
+        # Model prediction
+        prediction = self.model.predict(processed_data)
+        transcript = '...'  # Convert prediction to transcript
 
-# Open stream
-stream = audio_interface.open(format=FORMAT, channels=CHANNELS,
-                              rate=RATE, input=True,
-                              frames_per_buffer=CHUNK,
-                              stream_callback=stream_callback)
+        return (in_data, pyaudio.paContinue)
 
-# Start the stream
-stream.start_stream()
+    def start_stream(self):
+        """Starts the audio stream for real-time processing."""
+        self.stream = self.audio_interface.open(format=self.format, channels=self.channels,
+                                                rate=self.rate, input=True,
+                                                frames_per_buffer=self.chunk,
+                                                stream_callback=self.stream_callback)
+        self.stream.start_stream()
 
-# Keep the main thread alive while the stream is processed in the background
-try:
-    while stream.is_active():
-        pass
-except KeyboardInterrupt:
-    # Stop and close the stream
-    stream.stop_stream()
-    stream.close()
-    audio_interface.terminate()
+
+def main():
+    """
+    Main function to execute real-time audio processing and transcription.
+    """
+    model_path = 'path_to_your_model.h5'  # Replace with the actual model path
+    audio_processor = RealTimeAudioProcessor(model_path)
+
+    try:
+        audio_processor.start_stream()
+        while audio_processor.stream.is_active():
+            pass
+    except KeyboardInterrupt:
+        audio_processor.stream.stop_stream()
+        audio_processor.stream.close()
+        audio_processor.audio_interface.terminate()
+
+
+if __name__ == "__main__":
+    main()
--- model.py
+++ model.py
@@ -1,134 +1,93 @@
 import os
-import shutil
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.layers import LayerNormalization, MultiHeadAttention, Dense, Dropout
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
-import librosa  # for audio processing
+import librosa
 from spafe.features.gfcc import gfcc
 from audiomentations import Compose, AddGaussianNoise, TimeStretch, PitchShift, Shift
 
-# Update paths as needed
-DATASET_ROOT = '/Users/mruthunjai_govindaraju/Downloads/Data/malayalam/Female'
-AUDIO_SUBFOLDER = "Voices"
-NOISE_SUBFOLDER = "Noise"
-DATASET_AUDIO_PATH = os.path.join(DATASET_ROOT, AUDIO_SUBFOLDER)
-DATASET_NOISE_PATH = os.path.join(DATASET_ROOT, NOISE_SUBFOLDER)
-INDEX_TSV_PATH = os.path.join(DATASET_ROOT, 'index.tsv')
+class AudioDataProcessor:
+    """
+    Audio Data Processor for loading and preprocessing audio data.
 
-# Constants
-VALID_SPLIT = 0.1
-SHUFFLE_SEED = 43
-SAMPLING_RATE = 16000
-SCALE = 0.5
-BATCH_SIZE = 128
-EPOCHS = 100
-NUM_MFCC = 13  # Number of MFCCs to extract
+    Attributes:
+        dataset_root (str): Root directory of the dataset.
+        audio_subfolder (str): Subfolder containing audio files.
+        noise_subfolder (str): Subfolder containing noise files.
+        index_tsv_path (str): Path to the index TSV file.
+        sampling_rate (int): Sampling rate for audio processing.
+        num_mfcc (int): Number of Mel-frequency cepstral coefficients.
 
-# Load transcripts
-index_df = pd.read_csv(INDEX_TSV_PATH, sep='\t', header=None)
-index_df.columns = ['filename', 'transcript']
+    Methods:
+        load_index: Loads the index file and returns a DataFrame.
+        split_dataset: Splits the dataset into training and validation sets.
+    """
+    def __init__(self, dataset_root, audio_subfolder, noise_subfolder, index_tsv_path, sampling_rate=16000, num_mfcc=13):
+        self.dataset_root = dataset_root
+        self.audio_subfolder = audio_subfolder
+        self.noise_subfolder = noise_subfolder
+        self.index_tsv_path = index_tsv_path
+        self.sampling_rate = sampling_rate
+        self.num_mfcc = num_mfcc
 
-# SpecAugment Function
-def spec_augment(spec, num_mask=2, freq_masking_max_percentage=0.15, time_masking_max_percentage=0.3):
-    spec = tf.identity(spec)
-    for i in range(num_mask):
-        # Frequency masking
-        freq_mask = tf.cast(tf.random.uniform([], 0, freq_masking_max_percentage) * tf.shape(spec)[1], tf.int32)
-        f0 = tf.cast(tf.random.uniform([], 0, tf.shape(spec)[1] - freq_mask), tf.int32)
-        spec[:, f0:f0+freq_mask] = 0
+    def load_index(self):
+        """Loads the index TSV file."""
+        index_df = pd.read_csv(self.index_tsv_path, sep='\t', header=None)
+        index_df.columns = ['filename', 'transcript']
+        return index_df
 
-        # Time masking
-        time_mask = tf.cast(tf.random.uniform([], 0, time_masking_max_percentage) * tf.shape(spec)[0], tf.int32)
-        t0 = tf.cast(tf.random.uniform([], 0, tf.shape(spec)[0] - time_mask), tf.int32)
-        spec[t0:t0+time_mask, :] = 0
+    def split_dataset(self, valid_split=0.1, shuffle_seed=43):
+        """Splits the dataset into training and validation sets."""
+        index_df = self.load_index()
+        audio_paths = [os.path.join(self.dataset_root, self.audio_subfolder, fname) for fname in index_df['filename']]
+        labels = index_df['transcript']
+        rng = np.random.RandomState(shuffle_seed)
+        shuffled_indices = rng.permutation(len(audio_paths))
+        train_indices = shuffled_indices[:-int(valid_split * len(audio_paths))]
+        valid_indices = shuffled_indices[-int(valid_split * len(audio_paths)):]
 
-    return spec
+        train_audio_paths = [audio_paths[i] for i in train_indices]
+        train_labels = [labels[i] for i in train_indices]
+        valid_audio_paths = [audio_paths[i] for i in valid_indices]
+        valid_labels = [labels[i] for i in valid_indices]
 
-# Augmentation with audiomentations
-augment = Compose([
-    AddGaussianNoise(min_amplitude=0.001, max_amplitude=0.015, p=0.5),
-    TimeStretch(min_rate=0.8, max_rate=1.25, p=0.5),
-    PitchShift(min_semitones=-4, max_semitones=4, p=0.5),
-    Shift(min_fraction=-0.5, max_fraction=0.5, p=0.5),
-])
+        return train_audio_paths, train_labels, valid_audio_paths, valid_labels
 
-# Function to load and preprocess audio files
-def load_and_preprocess_audio(file_path, feature_type='log_mel', augment_data=False):
-    audio, _ = librosa.load(file_path, sr=SAMPLING_RATE)
-    if augment_data:
-        audio = augment(samples=audio, sample_rate=SAMPLING_RATE)
-    
-    if feature_type == 'log_mel':
-        mel_spec = librosa.feature.melspectrogram(y=audio, sr=SAMPLING_RATE, n_mels=40)
-        log_mel_spec = librosa.power_to_db(mel_spec)
-        return log_mel_spec.T
-    
-    elif feature_type == 'mfcc':
-        mfcc = librosa.feature.mfcc(y=audio, sr=SAMPLING_RATE, n_mfcc=NUM_MFCC)
-        return mfcc.T
 
-    elif feature_type == 'gfcc':
-        gfcc_features = gfcc(sig=audio, fs=sr, num_ceps=NUM_MFCC)
-        return gfcc_features
+class ConformerBlock(keras.layers.Layer):
+    """
+    Conformer block for the Conformer model.
 
-# Function to add noise
-def add_noise(audio):
-    noise_sample = tf.random.uniform((tf.shape(audio)[0],)) * 0.1
-    noisy_audio = audio + noise_sample
-    return noisy_audio
+    Attributes:
+        embed_dim (int): Embedding dimension.
+        num_heads (int): Number of heads in the multi-head attention.
+        ff_dim (int): Dimension of feed-forward network.
+        conv_kernel_size (int): Kernel size for the convolution layer.
+        rate (float): Dropout rate.
+        kernel_regularizer: Regularizer function applied to the `kernel` weights matrix.
 
-# Apply SpecAugment and Noise to the training dataset
-def apply_augmentations(features, labels):
-    features_augmented = spec_augment(features)
-    features_noisy = add_noise(features_augmented)
-    return features_noisy, labels
-
-# Function to convert paths to dataset
-def paths_to_dataset(audio_paths, labels, feature_type='log_mel'):
-    path_ds = tf.data.Dataset.from_tensor_slices(audio_paths)
-    audio_ds = path_ds.map(lambda x: tf.numpy_function(load_and_preprocess_audio, [x, feature_type], tf.float32), num_parallel_calls=tf.data.AUTOTUNE)
-    label_ds = tf.data.Dataset.from_tensor_slices(labels)
-    return tf.data.Dataset.zip((audio_ds, label_ds))
-
-# Creating datasets
-audio_paths = [os.path.join(DATASET_AUDIO_PATH, fname) for fname in index_df['filename']]
-labels = index_df['transcript']
-rng = np.random.RandomState(SHUFFLE_SEED)
-shuffled_indices = rng.permutation(len(audio_paths))
-train_indices = shuffled_indices[:-int(VALID_SPLIT * len(audio_paths))]
-valid_indices = shuffled_indices[-int(VALID_SPLIT * len(audio_paths)):]
-
-train_audio_paths = [audio_paths[i] for i in train_indices]
-train_labels = [labels[i] for i in train_indices]
-valid_audio_paths = [audio_paths[i] for i in valid_indices]
-valid_labels = [labels[i] for i in valid_indices]
-
-train_ds = paths_to_dataset(train_audio_paths, train_labels, feature_type='log_mel').map(apply_augmentations).batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
-valid_ds = paths_to_dataset(valid_audio_paths, valid_labels, feature_type='log_mel').batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
-
-# Transformer Encoder Block
-class ConformerBlock(tf.keras.layers.Layer):
+    Methods:
+        call: Performs the computation from inputs to outputs.
+    """
     def __init__(self, embed_dim, num_heads, ff_dim, conv_kernel_size=32, rate=0.1, kernel_regularizer=None):
         super(ConformerBlock, self).__init__()
-        self.ffn1 = tf.keras.layers.Dense(ff_dim, activation='relu', kernel_regularizer=kernel_regularizer)
-        self.ffn2 = tf.keras.layers.Dense(embed_dim, kernel_regularizer=kernel_regularizer)
-
+        self.ffn1 = Dense(ff_dim, activation='relu', kernel_regularizer=kernel_regularizer)
+        self.ffn2 = Dense(embed_dim, kernel_regularizer=kernel_regularizer)
         self.att = MultiHeadAttention(num_heads=num_heads, key_dim=embed_dim)
-        self.conv = tf.keras.layers.Conv1D(filters=2*embed_dim, kernel_size=conv_kernel_size, padding='same', activation='relu', kernel_regularizer=kernel_regularizer)
-        self.conv1 = tf.keras.layers.Conv1D(filters=embed_dim, kernel_size=1, activation='relu', kernel_regularizer=kernel_regularizer)
-
+        self.conv = keras.layers.Conv1D(filters=2*embed_dim, kernel_size=conv_kernel_size, padding='same', activation='relu', kernel_regularizer=kernel_regularizer)
+        self.conv1 = keras.layers.Conv1D(filters=embed_dim, kernel_size=1, activation='relu', kernel_regularizer=kernel_regularizer)
         self.layernorm1 = LayerNormalization(epsilon=1e-6)
         self.layernorm2 = LayerNormalization(epsilon=1e-6)
         self.layernorm3 = LayerNormalization(epsilon=1e-6)
-
         self.dropout1 = Dropout(rate)
         self.dropout2 = Dropout(rate)
         self.dropout3 = Dropout(rate)
 
     def call(self, inputs, training=False):
+        """Performs the computation of the Conformer block."""
         ffn_output = self.ffn1(inputs)
         ffn_output = self.ffn2(ffn_output)
         ffn_output = self.dropout1(ffn_output, training=training)
@@ -145,79 +104,195 @@ class ConformerBlock(tf.keras.layers.Layer):
 
         return out3
 
-# Building the Conformer Model with Regularization
-def build_conformer_model(hp, input_shape=(None, 40), num_classes=None):
-    num_conformer_blocks = hp.get('num_conformer_blocks', 4)
-    embed_dim = 128
-    num_heads = 4
-    ff_dim = 128
-    conv_kernel_size = 32
-    kernel_regularizer = tf.keras.regularizers.l2(0.001)
 
-    inputs = keras.layers.Input(shape=input_shape)
-    x = inputs
+class ConformerModel:
+    """
+    Conformer Model for speech recognition.
 
-    for _ in range(num_conformer_blocks):
-        x = ConformerBlock(embed_dim, num_heads, ff_dim, conv_kernel_size, kernel_regularizer=kernel_regularizer)(x)
+    Attributes:
+        input_shape (tuple): Shape of the input data.
+        num_classes (int): Number of output classes.
 
-    x = keras.layers.GlobalAveragePooling1D()(x)
-    x = Dense(256, activation='relu', kernel_regularizer=kernel_regularizer)(x)
-    x = Dropout(0.5)(x)
-    x = Dense(128, activation='relu', kernel_regularizer=kernel_regularizer)(x)
-    outputs = Dense(num_classes, activation='softmax', kernel_regularizer=kernel_regularizer)(x)
+    Methods:
+        build_model: Builds the Conformer model.
+    """
+    def __init__(self, input_shape=(None, 40), num_classes=None):
+        self.input_shape = input_shape
+        self.num_classes = num_classes
 
-    model = keras.models.Model(inputs, outputs)
-    model.compile(optimizer=keras.optimizers.get(hp.get('optimizer', 'adam')),
-                loss='sparse_categorical_crossentropy',
-                metrics=['accuracy'])
-    return model
+    def build_model(self, hp):
+        """Builds the Conformer model with hyperparameters."""
+        num_conformer_blocks = hp.get('num_conformer_blocks', 4)
+        embed_dim = 128
+        num_heads = 4
+        ff_dim = 128
+        conv_kernel_size = 32
+        kernel_regularizer = keras.regularizers.l2(0.001)
 
-num_classes = len(np.unique(labels))
-# Define hyperparameter grid
-hyperparameter_grid = {
-    'learning_rate': [0.001, 0.0001],
-    'optimizer': ['adam', 'rmsprop'],
-    'num_conformer_blocks': [3, 4, 5],
-}
+        inputs = keras.layers.Input(shape=self.input_shape)
+        x = inputs
+        for _ in range(num_conformer_blocks):
+            x = ConformerBlock(embed_dim, num_heads, ff_dim, conv_kernel_size, kernel_regularizer=kernel_regularizer)(x)
 
-# Create a Keras Tuner object
-tuner = keras.tuners.BayesianOptimization(
-    build_conformer_model,
-    objective='val_accuracy',
-    max_trials=10,
-    hyperparameters=hyperparameter_grid,
-)
+        x = keras.layers.GlobalAveragePooling1D()(x)
+        x = Dense(256, activation='relu', kernel_regularizer=kernel_regularizer)(x)
+        x = Dropout(0.5)(x)
+        x = Dense(128, activation='relu', kernel_regularizer=kernel_regularizer)(x)
+        outputs = Dense(self.num_classes, activation='softmax', kernel_regularizer=kernel_regularizer)(x)
 
-# Early stopping and learning rate reduction callbacks
-early_stopping = EarlyStopping(monitor='val_loss', patience=5)
-reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3)
+        model = keras.models.Model(inputs, outputs)
+        model.compile(optimizer=keras.optimizers.get(hp.get('optimizer', 'adam')),
+                    loss='sparse_categorical_crossentropy',
+                    metrics=['accuracy'])
+        return model
 
-# Train the model with hyperparameter tuning
-tuner.search(
-    train_ds,
-    validation_data=valid_ds,
-    epochs=EPOCHS,
-    callbacks=[early_stopping, reduce_lr],
-)
+class AudioAugmenter:
+    """
+    Audio Augmenter for applying various augmentations to audio data.
 
-# Get the best hyperparameter configuration
-best_hyperparameters = tuner.get_best_hyperparameters(num_trials=1)[0]
+    Methods:
+        spec_augment: Applies SpecAugment to the given spectrogram.
+        add_noise: Adds noise to the audio.
+        augment: Applies a series of augmentations using audiomentations.
+    """
+    @staticmethod
+    def spec_augment(spec, num_mask=2, freq_masking_max_percentage=0.15, time_masking_max_percentage=0.3):
+        """Applies SpecAugment to the given spectrogram."""
+        spec = tf.identity(spec)
+        for i in range(num_mask):
+            freq_mask = tf.cast(tf.random.uniform([], 0, freq_masking_max_percentage) * tf.shape(spec)[1], tf.int32)
+            f0 = tf.cast(tf.random.uniform([], 0, tf.shape(spec)[1] - freq_mask), tf.int32)
+            spec[:, f0:f0+freq_mask] = 0
 
-# Build and train the model with the best hyperparameters
-model = build_conformer_model(input_shape, num_classes, **best_hyperparameters)
+            time_mask = tf.cast(tf.random.uniform([], 0, time_masking_max_percentage) * tf.shape(spec)[0], tf.int32)
+            t0 = tf.cast(tf.random.uniform([], 0, tf.shape(spec)[0] - time_mask), tf.int32)
+            spec[t0:t0+time_mask, :] = 0
 
-model.fit(train_ds, validation_data=valid_ds, epochs=EPOCHS)
+        return spec
 
-# Evaluate the model with the best hyperparameters
-model.evaluate(valid_ds)
+    @staticmethod
+    def add_noise(audio):
+        """Adds noise to the audio."""
+        noise_sample = tf.random.uniform((tf.shape(audio)[0],)) * 0.1
+        noisy_audio = audio + noise_sample
+        return noisy_audio
 
-model = build_conformer_model((None, 40), num_classes)  # Adjust input shape based on feature extraction
+    @staticmethod
+    def augment():
+        """Applies a series of augmentations using audiomentations."""
+        return Compose([
+            AddGaussianNoise(min_amplitude=0.001, max_amplitude=0.015, p=0.5),
+            TimeStretch(min_rate=0.8, max_rate=1.25, p=0.5),
+            PitchShift(min_semitones=-4, max_semitones=4, p=0.5),
+            Shift(min_fraction=-0.5, max_fraction=0.5, p=0.5),
+        ])
 
-model.compile(optimizer='Adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.summary()
 
-# Training the model
-history = model.fit(train_ds, epochs=EPOCHS, validation_data=valid_ds)
+class AudioFeatureExtractor:
+    """
+    Audio Feature Extractor for extracting features from audio data.
 
-# Evaluate the model
-print(model.evaluate(valid_ds))
+    Methods:
+        load_and_preprocess_audio: Loads and preprocesses audio files.
+        apply_augmentations: Applies augmentations to features and labels.
+        paths_to_dataset: Converts paths to a TensorFlow dataset.
+    """
+    @staticmethod
+    def load_and_preprocess_audio(file_path, feature_type='log_mel', augment_data=False, sampling_rate=16000):
+        """Loads and preprocesses audio files."""
+        audio, _ = librosa.load(file_path, sr=sampling_rate)
+        if augment_data:
+            audio = AudioAugmenter.augment()(samples=audio, sample_rate=sampling_rate)
+
+        if feature_type == 'log_mel':
+            mel_spec = librosa.feature.melspectrogram(y=audio, sr=sampling_rate, n_mels=40)
+            log_mel_spec = librosa.power_to_db(mel_spec)
+            return log_mel_spec.T
+
+        elif feature_type == 'mfcc':
+            mfcc = librosa.feature.mfcc(y=audio, sr=sampling_rate, n_mfcc=13)
+            return mfcc.T
+
+        elif feature_type == 'gfcc':
+            gfcc_features = gfcc(sig=audio, fs=sampling_rate, num_ceps=13)
+            return gfcc_features
+
+    @staticmethod
+    def apply_augmentations(features, labels):
+        """Applies augmentations to features and labels."""
+        features_augmented = AudioAugmenter.spec_augment(features)
+        features_noisy = AudioAugmenter.add_noise(features_augmented)
+        return features_noisy, labels
+
+    @staticmethod
+    def paths_to_dataset(audio_paths, labels, feature_type='log_mel', sampling_rate=16000):
+        """Converts paths to a TensorFlow dataset."""
+        path_ds = tf.data.Dataset.from_tensor_slices(audio_paths)
+        audio_ds = path_ds.map(lambda x: tf.numpy_function(
+            lambda file_path: AudioFeatureExtractor.load_and_preprocess_audio(
+                file_path, feature_type, False, sampling_rate
+            ), [x], tf.float32
+        ), num_parallel_calls=tf.data.AUTOTUNE)
+        label_ds = tf.data.Dataset.from_tensor_slices(labels)
+        return tf.data.Dataset.zip((audio_ds, label_ds))
+
+
+class HyperparameterTuner:
+    """
+    Hyperparameter Tuner using Keras Tuner.
+
+    Attributes:
+        build_model_fn: Function to build the model.
+        objective (str): Objective for the tuner to optimize.
+        max_trials (int): Maximum number of trials for hyperparameter tuning.
+        hyperparameter_grid (dict): Hyperparameter grid for tuning.
+
+    Methods:
+        create_tuner: Creates a tuner instance.
+    """
+    def __init__(self, build_model_fn, objective, max_trials, hyperparameter_grid):
+        self.build_model_fn = build_model_fn
+        self.objective = objective
+        self.max_trials = max_trials
+        self.hyperparameter_grid = hyperparameter_grid
+
+    def create_tuner(self):
+        """Creates a tuner instance."""
+        return keras.tuners.BayesianOptimization(
+            self.build_model_fn,
+            objective=self.objective,
+            max_trials=self.max_trials,
+            hyperparameters=self.hyperparameter_grid,
+        )
+
+
+class ConformerTrainer:
+    """
+    Trainer for Conformer Model.
+
+    Attributes:
+        model (keras.Model): The Conformer model to be trained.
+        train_data (tf.data.Dataset): Training data.
+        valid_data (tf.data.Dataset): Validation data.
+        epochs (int): Number of epochs to train.
+
+    Methods:
+        train_model: Trains the model.
+        evaluate_model: Evaluates the model.
+    """
+    def __init__(self, model, train_data, valid_data, epochs):
+        self.model = model
+        self.train_data = train_data
+        self.valid_data = valid_data
+        self.epochs = epochs
+
+    def train_model(self):
+        """Trains the Conformer model."""
+        early_stopping = EarlyStopping(monitor='val_loss', patience=5)
+        reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3)
+        self.model.fit(self.train_data, validation_data=self.valid_data, epochs=self.epochs, callbacks=[early_stopping, reduce_lr])
+
+    def evaluate_model(self):
+        """Evaluates the Conformer model."""
+        return self.model.evaluate(self.valid_data)
+
